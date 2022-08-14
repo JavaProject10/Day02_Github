965d211e81a42ceb1a00e8f02baca5cc0874a500
Java project <java100class@gmail.com>
2022-08-14 11:32:13 +0300

    2.C

--- src/Day01_Git.java
+++ src/Day01_Git.java
@@ -1,6 +1,7 @@
 public class Day01_Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Deneme");
 
     }
 }
